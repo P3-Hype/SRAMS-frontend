35857c814e00f9ea1b864bea0a3b6c87e0c1e810
alexrefshauge <arefsh22@student.aau.dk>
2023-10-08 21:52:39 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     coverage: {
       reporter: ['text', 'json-summary', 'json', 'lcov'],
       provider: 'istanbul', // or v8
+      exclude: ['./src/test/**', '**/node_modules/**', '**/dist/**', '**/cypress/**', '**/.{idea,git,cache,output,temp}/**', '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*']
     },
     environment: 'jsdom',
     globals: true,
