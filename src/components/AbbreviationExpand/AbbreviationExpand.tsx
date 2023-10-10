8b4690b42f1fcf0be64c36b026982e3ae68bbc94
alexrefshauge <arefsh22@student.aau.dk>
2023-10-10 15:25:08 +0200

    resolve comments

--- src/components/AbbreviationExpand/AbbreviationExpand.tsx
+++ src/components/AbbreviationExpand/AbbreviationExpand.tsx
@@ -30,13 +30,13 @@ interface WordProps {
 
 function Word(props: WordProps ) {
     const firstLetter = props.word[0];
-    const isConjunction = firstLetter != firstLetter.toLocaleUpperCase()
-    const hiddenPart = isConjunction ? props.word : props.word.substring(1, props.word.length);
+    const collapses = firstLetter != firstLetter.toLocaleUpperCase();
+    const hiddenPart = collapses ? props.word : props.word.substring(1, props.word.length);
     return (
         <Box display={"flex"} flexDirection={"row"} alignItems={"center"}>
-            {!isConjunction && <span className="first-letter">{firstLetter}</span>}
-            {!isConjunction && <span style={{transition: ".5s", opacity: props.expanded ? "0%" : "100%"}}>.</span>}
-            <div style={{maxWidth: props.expanded ? "100px" : "0px", transition: ".5s ease", marginRight: (isConjunction && !props.expanded) ? "0px" : "1rem"}}>
+            {!collapses && <span className="first-letter">{firstLetter}</span>}
+            {!collapses && <span style={{transition: ".5s", opacity: props.expanded ? "0%" : "100%"}}>.</span>}
+            <div style={{maxWidth: props.expanded ? "100px" : "0px", transition: ".5s ease", marginRight: (collapses && !props.expanded) ? "0px" : "1rem"}}>
                 <Grow in={props.expanded} timeout={500}>
                     <span>{hiddenPart}</span>
                 </Grow>
