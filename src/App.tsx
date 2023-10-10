7bfa24d076e8a1eecd7bdc6fbf135fdb666cc9ea
Tobias Agerskov Bay <tobiasabay@gmail.com>
2023-10-10 17:09:09 +0200

    Deleted Smells

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import './App.css';
-import './components/Header/Header';
 import Header from './components/Header/Header';
 
 function App() {
 
   return (
-    <>
       <Header/>
-    </>
   )
 }
 
