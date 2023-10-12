e3c33cfd28c44513326bf6361b38d1e21271aa87
alexrefshauge <arefsh22@student.aau.dk>
2023-10-12 13:42:34 +0200

    remove alert

--- src/components/BasePage/BasePage.tsx
+++ src/components/BasePage/BasePage.tsx
@@ -1,30 +1,16 @@
-import { Alert, AlertColor, Box, IconButton, Snackbar } from "@mui/material";
+import { Box } from "@mui/material";
 import { ReactNode } from "react";
 
 interface BasePageProps {
     children: ReactNode,
-    alertOpen: boolean,
-    alertMessage: string,
-    alertSeverity: AlertColor,
-    alertCloseFunction: () => void
 }
 
 export function BasePage(props: BasePageProps) {
-
-    const handleClose = () => {
-        props.alertCloseFunction();
-    }
-
     return (
         <>
             <Box flex={'row'}>
                 {props.children}
             </Box>
-            <Snackbar autoHideDuration={6000} onClose={handleClose}>
-                <>
-                    <Alert severity={props.alertSeverity}>{props.alertMessage}</Alert>
-                </>
-            </Snackbar>
         </>
     );
 }
