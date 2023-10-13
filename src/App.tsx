adb3e31c585a7a7bd0c18a986bbde8abf534399f
alexrefshauge <arefsh22@student.aau.dk>
2023-10-13 11:30:01 +0200

    fix imports in App

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Header from './components/Header/Header';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import theme from './theme';
 import HomePage from './pages/HomePage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
 function App() {
