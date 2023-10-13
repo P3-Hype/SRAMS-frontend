a8b606a715c72d6fb181f8f0038202a339a16128
Androkless <NO01VU@student.aau.dk>
2023-10-13 12:26:05 +0200

    add NoPage

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './pages/HomePage';
 import RoomAdministrationPage from './pages/RoomAdministrationPage';
 import RoomOverviewPage from './pages/RoomOverviewPage';
+import NoPage from './pages/NoPage';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="admin" element={<RoomAdministrationPage/>} />
           <Route path="overview" element={<RoomOverviewPage/>} />
+          <Route path="*" element={<NoPage/>} />
         </Routes>
       </BrowserRouter>
     </>
