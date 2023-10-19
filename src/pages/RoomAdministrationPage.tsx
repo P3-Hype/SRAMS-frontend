469dae4b3691e1d67f63074508432942b964174a
Tobias Bay <tbay22@student.aau.dk>
2023-10-19 14:13:51 +0200

    Removed box and cardaction

--- src/pages/RoomAdministrationPage.tsx
+++ src/pages/RoomAdministrationPage.tsx
@@ -4,7 +4,7 @@ import useAlert from "../hooks/useAlert";
 
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import SettingsIcon from '@mui/icons-material/Settings';
-import { Box, Button, CardActions, Chip, Container, Stack } from "@mui/material";
+import { Box, Button, Chip, Container, Stack } from "@mui/material";
 import Accordion from '@mui/material/Accordion';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import AccordionSummary from '@mui/material/AccordionSummary';
@@ -36,9 +36,7 @@ function RoomAdministrationPage() {
                     {rooms.map(r=> (
                         <Accordion>
                             <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
-                                <Box sx={{alignItems:"center"}}>
                                     <Typography>{r.name}</Typography>
-                                </Box>
                                 <Stack direction={"row-reverse"} gap={2} sx={{width:"100%", alignItems:"center"}}>
                                     <Button variant="contained">
                                         <SettingsIcon />
@@ -58,11 +56,9 @@ function RoomAdministrationPage() {
                     ))}
 
                     
-                    <CardActions sx={{ justifyContent: "center" }}>
                         <Button size="large" variant="contained" color="primary" fullWidth onClick={addClickHandeler}>
                             <Typography variant="h6">Add more Roms</Typography>
                         </Button>
-                    </CardActions>
 
                 </Box>
             </Container>
