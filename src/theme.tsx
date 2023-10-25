e8d6b6f1e49b02f99a4eed4e8e4f77889ab38d44
Tobias Agerskov Bay <tobiasabay@gmail.com>
2023-10-25 10:12:22 +0200

    Fixed bug

--- src/theme.tsx
+++ src/theme.tsx
@@ -35,7 +35,7 @@ export const theme = createTheme({
     },
     MuiAccordionSummary: {
       styleOverrides: {
-        content: ({}) => ({
+        content: ({ theme }) => ({
           display: "flex",
           flexDirection: "row",
           justifyContent: "space-between",
