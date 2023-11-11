c3476e64d6343806d558d775714bc064e11635f3
MathiasFM <mathiasmogensen1@gmail.com>
2023-11-11 15:45:56 +0100

    fixed the docker build :))))))

--- htpasswd.sh
+++ htpasswd.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sh -c "echo -n 'srams :' >> /etc/nginx/.htpasswd"
+sh -c "echo -n 'srams:' >> /etc/nginx/.htpasswd"
 sh -c 'echo "$(openssl passwd -apr1 srams)" >> /etc/nginx/.htpasswd'
