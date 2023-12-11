677a30a776ef91990bfe6a77ba9c97f957e44aa9
MathiasFM <mathiasmogensen1@gmail.com>
2023-12-11 23:09:34 +0100

    remove loading feature

--- src/components/HomePage/NavButtons.tsx
+++ src/components/HomePage/NavButtons.tsx
@@ -3,8 +3,6 @@ import Button from '@mui/material/Button';
 import { Link as RouterLink } from 'react-router-dom';
 import Link from '@mui/material/Link';
 import { useTheme } from '@mui/material/styles';
-import { CircularProgress } from '@mui/material';
-import { useState } from 'react';
 
 /**
  * NavButtons is a component function that returns 2 styled navigation buttons,
@@ -38,53 +36,30 @@ export default function NavButtons() {
 		minHeight: '100vh',
 	};
 
-	const [isLoading, setIsLoading] = useState(false);
-
 	function refreshPage() {
-		setIsLoading(true);
 		setTimeout(()=>{
 			window.location.reload();
 		}, 1);
 	}
 
 	return (
-		<>
-			{isLoading && (
-				<div
-				style={{
-					position: 'fixed',
-					top: 0,
-					left: 0,
-					width: '100%',
-					height: '100%',
-					background: 'rgba(0, 0, 0, 0.5)',
-					display: 'flex',
-					alignItems: 'center',
-					justifyContent: 'center',
-					zIndex: 9999,
-				}}
-				>
-				<CircularProgress color="inherit" />
-				</div>
-			)}
-			<Grid container spacing={20} style={GridStyle}>
-				<Grid item>
-					<Link component={RouterLink} to='/admin' onClick={refreshPage}>
-						<Button sx={buttonStyle}>Admin</Button>
-					</Link>
-				</Grid>
-				<Grid item>
-					<Link component={RouterLink} to='/overview'>
-						<Button sx={buttonStyle}>Manage Bookings</Button>
-					</Link>
-				</Grid>
-				<Grid item>
-					<Link component={RouterLink} to='/dashboard'>
-						<Button sx={buttonStyle}>Dashboard</Button>
-					</Link>
-				</Grid>
+		<Grid container spacing={20} style={GridStyle}>
+			<Grid item>
+				<Link component={RouterLink} to='/admin' onClick={refreshPage}>
+					<Button sx={buttonStyle}>Admin</Button>
+				</Link>
 			</Grid>
-		</>
+			<Grid item>
+				<Link component={RouterLink} to='/overview'>
+					<Button sx={buttonStyle}>Manage Bookings</Button>
+				</Link>
+			</Grid>
+			<Grid item>
+				<Link component={RouterLink} to='/dashboard'>
+					<Button sx={buttonStyle}>Dashboard</Button>
+				</Link>
+			</Grid>
+		</Grid>
 	);
 }
 
